f6dbdc6f65f67572a931b5d8c9b9dbce933e95d8
Eliseu Codinhoto <zeucxb@gmail.com>
2018-06-24 04:31:28 -0300

    Make this a real cli

---
+++ bin/main.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+const program = require('commander');
+const app = require('../src/app');
+
+program
+  .version('0.1.0')
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
+
+console.log('you ordered a pizza with:');
+if (program.peppers) console.log('  - peppers');
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbqSauce) console.log('  - bbq');
+console.log('  - %s cheese', program.cheese);
+
+app();
--- src/core.js
+++ src/core.js
@@ -1,17 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser')
 const R = require('ramda');
-
-const express = require('express');
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const localDir = process.cwd();
 
 const checkUrlConfig = () => {
   try {
-    return require('./url.config.json');
+    return require(`${localDir}/url.config.json`);
   } catch (err) {
     return false;
   }
@@ -19,23 +11,9 @@ const checkUrlConfig = () => {
 
 const urlConfig = (checkUrlConfig)();
 
-const sendResponse = (strPath, res) => {
-  const filePath = path.join(__dirname, strPath);
-
-  try {
-    const jsonFile = require(filePath);
-    res.status(200).json(jsonFile);
-    console.log(`Response: 200 ${filePath}`);
-  } catch (err) {
-    console.log(err);
-    res.status(404).send(`File '${filePath}' Not Found!`);
-    console.log("Response: 404 ${filePath}, err");
-  }
-}
-
 const paramsResponse = (url, params, res) => {
   const strPath = `${url}/config.js`;
-  const filePath = path.join(__dirname, strPath);
+  const filePath = `${localDir}/${strPath}`;
 
   try {
     const configFile = require(filePath);
@@ -81,18 +59,14 @@ const parseUrl = (url) => {
 
     if (!urlMatch) throw new Error('Invalid resquest url');
 
-    console.log(urlMatch);
-
     return urlMatch.path;
   } else {
     return `url/${url}`;
   }
 }
 
-app.get('/robots.txt', (req, res) => res.sendStatus(200));
-app.all('*', (req, res) => {
-  console.log(`Request came: ${req.url}`);
-  responseControl(req.method, parseUrl(req.path), req.body, res);
-});
-
-app.listen(3000);
+module.exports = {
+  paramsResponse,
+  parseUrl,
+  responseControl,
+};
